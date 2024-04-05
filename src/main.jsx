57f3561f515a44cbc453781284d4649d6064c002
Sujal Vishwkarma <125433035+Sujalvishwkarma@users.noreply.github.com>
2024-04-05 19:09:35 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./assets/css/index.css";
 import DashboardLayout from "./layout/dashboard";
 import PageLayout from "./layout/page";
-import Home from "./pages/dashboard/home";
+import Home from "./pages/dashboard/Home";
 import TeacherRequest from "./pages/dashboard/teacherrequest";
 import TeacherRegister from "./pages/dashboard/teacherregister";
 import StudentRequest from "./pages/dashboard/studentrequests";
