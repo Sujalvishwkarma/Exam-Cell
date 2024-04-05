cf5104eee13ad44db32650bd8f0ffef0baa080df
Sujal Vishwkarma <125433035+Sujalvishwkarma@users.noreply.github.com>
2024-04-05 21:10:21 +0530

    Update dashboardheader.jsx

--- src/components/dashboardheader.jsx
+++ src/components/dashboardheader.jsx
@@ -4,7 +4,7 @@ function DashboardHeader(props)
 {
     return (
         <>
-        <div className="py-4 px-8 flex items-center justify-between bg-blue-600 text-white max-md:flex-col">
+        <div className="fixed top-0 w-full py-4 px-8 flex items-center justify-between bg-blue-600 text-white max-md:flex-col">
             <div className="text-2xl">Examination Cell</div>
             <div className="">{!props.student?'Student Name':props.student}</div>
         </div>
